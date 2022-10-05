9e6a00060c25c4efaacfa4835f3d73e4fc2a11ce
Asecave <39377667+Asecave@users.noreply.github.com>
2022-10-05 17:05:59 +0200

    viewport change on scroll

--- core/src/com/asecave/chipper/Grid.java
+++ core/src/com/asecave/chipper/Grid.java
@@ -1,32 +1,42 @@
 package com.asecave.chipper;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Grid {
-	
+
 	Tile[][] tiles;
-	
+
 	private int scale = 10;
 
 	public Grid(int width, int height) {
-		
+
 		tiles = new Tile[width][height];
-	
+
 	}
-	
+
 	public void render(ShapeRenderer sr) {
-		
-		sr.setColor(Color.DARK_GRAY);
+
 		sr.set(ShapeType.Filled);
 		for (int x = 0; x < tiles.length; x++) {
 			for (int y = 0; y < tiles[0].length; y++) {
+				sr.setColor(Color.DARK_GRAY);
 				sr.rect(x * scale, y * scale, scale, 1);
 				sr.rect(x * scale, y * scale, 1, scale);
+				int mx = (int) ((Gdx.input.getX() / Main.cam.viewportWidth * Gdx.graphics.getWidth()) + Main.cam.position.x - Gdx.graphics.getWidth() / 2);
+				int my = (int) (Gdx.graphics.getHeight() - Gdx.input.getY() + Main.cam.position.y - Gdx.graphics.getHeight() / 2);
+				if (mx >= x * scale && mx < x * scale + scale && my >= y * scale
+						&& my < y * scale + scale) {
+					sr.setColor(Color.GRAY);
+					sr.rect(x * scale + 1, y * scale + 1, scale - 1, scale - 1);
+				}
 			}
 		}
-		
+		sr.rect(tiles.length * scale, 0, 1, tiles[0].length * scale);
+		sr.rect(0, tiles[0].length * scale, tiles.length * scale, 1);
+
 		for (int x = 0; x < tiles.length; x++) {
 			for (int y = 0; y < tiles[0].length; y++) {
 				if (tiles[x][y] != null) {
--- core/src/com/asecave/chipper/Main.java
+++ core/src/com/asecave/chipper/Main.java
@@ -1,7 +1,5 @@
 package com.asecave.chipper;
 
-import org.w3c.dom.events.MouseEvent;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Buttons;
@@ -12,57 +10,67 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
-	
+
 	private ShapeRenderer sr;
-	private OrthographicCamera cam;
+	public static OrthographicCamera cam;
 	private InputAdapter input = new InputAdapter() {
 		public boolean scrolled(float amountX, float amountY) {
-			
+			Main.this.scrolled((int) amountY);
 			return true;
 		};
 	};
-	
+
 	private Grid grid;
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		sr = new ShapeRenderer();
 		sr.setAutoShapeType(true);
-		
+
 		cam = new OrthographicCamera();
-		
+
 		grid = new Grid(100, 100);
+
+		Gdx.input.setInputProcessor(input);
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1f);
-		
-		if (Gdx.input.isButtonPressed(Buttons.LEFT)) {
-			cam.position.x -= Gdx.input.getDeltaX();
-			cam.position.y += Gdx.input.getDeltaY();
+
+		if (Gdx.input.isButtonPressed(Buttons.RIGHT)) {
+			cam.position.x -= Gdx.input.getDeltaX() * (cam.viewportWidth / Gdx.graphics.getWidth());
+			cam.position.y += Gdx.input.getDeltaY() * (cam.viewportHeight / Gdx.graphics.getHeight());
 		}
-		
+
 		cam.update();
-		
+
 		sr.setProjectionMatrix(cam.combined);
 		sr.begin();
 		grid.render(sr);
 		sr.end();
-		
+
 		if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
 			Gdx.app.exit();
 		}
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		sr.dispose();
 	}
-	
+
 	@Override
 	public void resize(int width, int height) {
 		cam.viewportWidth = width;
 		cam.viewportHeight = height;
+	}
+
+	private void scrolled(int dir) {
+		float ar = cam.viewportWidth / cam.viewportHeight;
+		if (cam.viewportWidth + dir * 100 > 10 && cam.viewportWidth + dir * 100 <= Gdx.graphics.getWidth()) {
+			cam.viewportWidth += dir * 100;
+			cam.viewportHeight += (dir * 100) / ar;
+		}
 	}
 }
